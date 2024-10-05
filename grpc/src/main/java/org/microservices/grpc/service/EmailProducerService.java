bafe26d2c210b7164691540fe1e1f6658945dd2e
ahmed.zubairu <bazubairu@st.ug.edu.gh>
2024-10-05 15:27:33 +0000

    feat: add git ignore

--- grpc/src/main/java/org/microservices/grpc/service/EmailProducerService.java
+++ grpc/src/main/java/org/microservices/grpc/service/EmailProducerService.java
@@ -1,18 +1,22 @@
 package org.microservices.grpc.service;
 
-import org.microservices.grpc.EmailMessage;
+import org.microservices.grpc.dto.EmailMessage;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @Service
 public class EmailProducerService {
+    private static final Logger logger = LoggerFactory.getLogger(EmailProducerService.class);
+
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
     private static final String EMAIL_QUEUE = "emailQueue";
 
     public void sendEmail(EmailMessage emailMessage) {
+        logger.info("Sending email message to queue: {} and queue is {}", emailMessage, EMAIL_QUEUE);
         rabbitTemplate.convertAndSend(EMAIL_QUEUE, emailMessage);
     }
 }
