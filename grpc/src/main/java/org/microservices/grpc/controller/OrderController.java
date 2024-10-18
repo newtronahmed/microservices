d4b37bca773f0382fe6aec64793145ad11659188
ahmed.zubairu <bazubairu@st.ug.edu.gh>
2024-10-18 16:01:28 +0000

    feat: add pdf

--- grpc/src/main/java/org/microservices/grpc/controller/OrderController.java
+++ grpc/src/main/java/org/microservices/grpc/controller/OrderController.java
@@ -4,18 +4,18 @@ package org.microservices.grpc.controller;
 
 import org.microservices.grpc.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 //import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
+@RequestMapping("/api/orders")
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
 
-    @GetMapping("/orders")
+    @GetMapping
     public String createOrder(@RequestParam int productId, @RequestParam int quantity) {
         return orderService.createOrder(productId, quantity);
     }
